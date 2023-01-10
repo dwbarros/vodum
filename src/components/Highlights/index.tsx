ca7d50772cb43afa45b5f42bc3289e4ba73f5984
David Barros <dwbarros@gmail.com>
2023-01-10 14:41:47 +0000

    Remove text coming soog from Highlights and change Footer links URLs.

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,4 +1,3 @@
-import imgTitle from '../../assets/text_comingSoon.svg';
 import { NFTImage } from '../NFTImage';
 import { Container } from './styles';
 
@@ -6,7 +5,6 @@ import { Container } from './styles';
 export default function Highlights() {
     return (
         <Container>
-            <img src={ imgTitle } alt="Coming soon" />
             <NFTImage />
         </Container>
     )
